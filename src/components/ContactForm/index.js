1bf8ca858bfc4d42ab2205756ab716d44ff0b051
Yãry Castilho <andradeya4@gmail.com>
2025-09-11 09:25:17 -0300

    feat: Adicionando Spinner para campo de categoria

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -17,6 +17,7 @@ export default function ContactForm({ buttonLabel }) {
   const [phone, setPhone] = useState('')
   const [categoryId, setCategoryId] = useState('')
   const [categories, setCategories] = useState([])
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true)
 
   const { setError, removeError, getErrorMessageByFieldName, errors } = useErrors()
 
@@ -28,7 +29,9 @@ export default function ContactForm({ buttonLabel }) {
         const categoriesList = await CategoriesService.listCategories()
 
       setCategories(categoriesList)
-      }catch {}
+      }catch {} finally {
+        setIsLoadingCategories(false)
+      }
     }
 
     loadCategories()
@@ -96,10 +99,11 @@ export default function ContactForm({ buttonLabel }) {
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup isLoading={isLoadingCategories}>
         <Select
         value={categoryId}
         onChange={(event) => setCategoryId(event.target.value)}
+        disabled={isLoadingCategories}
         >
 
           <option value="">Sem categoria</option>
