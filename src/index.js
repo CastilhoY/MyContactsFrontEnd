2f7669f35a94f1b4a8c7174ea2b9b261407cdd7e
Yãry Castilho <andradeya4@gmail.com>
2025-09-22 08:51:51 -0300

    feat: adicionando a lib de eventmenager

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import './lib/EventMenager'
+
 ReactDOM.render(
     <React.StrictMode>
       <App />
---
+++ src/lib/EventMenager.js
@@ -0,0 +1,37 @@
+export default class EventMenager {
+  constructor() {
+    this.listners = new Map();
+  }
+
+  on(event, listner) {
+    if (!this.listners.has(event)) {
+      this.listners.set(event, []);
+    }
+
+    this.listners.get(event).push(listner);
+  }
+
+  emit(event, payload) {
+    if (!this.listners.has(event)) {
+      return;
+    }
+
+    this.listners.get(event).forEach((listner) => {
+      listner(payload);
+    });
+  }
+
+  removeListener(event, listnerToRemove) {
+    const listeners = this.listners.get(event);
+
+    if (!listeners) {
+      return;
+    }
+
+    const filteredLiseners = listeners.filter(
+      (listener) => listener !== listnerToRemove,
+    );
+
+    this.listeners.set(event, filteredLiseners)
+  }
+}
